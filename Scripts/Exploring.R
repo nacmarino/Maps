ed09fdd6bc9d07b236df6752587da4e907fbd7de
Nicholas Marino <nac.marino@gmail.com>
2015-10-06 23:49:46 -0300

    it is alive

--- Scripts/Exploring.R
+++ Scripts/Exploring.R
@@ -12,4 +12,9 @@ library(maptools)
 
 # first map of Brazil -----------------------------------------------------
 
-brazil_file <- readShapePoly("Layers/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp")
+brazil_file <- readShapePoly("Layers/BRA_adm/BRA_adm1.shp")
+br1 <- subset(brazil_file, brazil_file@data$ADMIN == "Brazil")
+br2 <- fortify(br1)
+head(br2)
+
+ggplot() + geom_path(data = br2, aes(x = long, y = lat, group = group), colour = "black", fill = NA)
