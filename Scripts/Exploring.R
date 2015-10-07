047c4a2b53ea5593434191e84cabfa2ef3657ff6
Nicholas Marino <nac.marino@gmail.com>
2015-10-07 00:14:40 -0300

    added rivers

--- Scripts/Exploring.R
+++ Scripts/Exploring.R
@@ -22,7 +22,11 @@ estados <- readShapePoly("Layers/BRA_adm/BRA_adm1.shp")
 estados1 <- fortify(estados)
 head(estados1)
 
-brazil + geom_path(data = estados1, aes(x = long, y = lat, group = group), colour = "black")
-brazi
+br_est <- brazil + geom_path(data = estados1, aes(x = long, y = lat, group = group), colour = "black")
+br_est
 
-river_file <- readShapePoly()
+river_file <- readShapePoly("Layers/BRA_wat/BRA_water_areas_dcw.shp")
+rios <- fortify(river_file)
+head(rios)
+
+br_est + geom_path(data = rios, aes(x = long, y = lat, group = group), color = "blue3")
