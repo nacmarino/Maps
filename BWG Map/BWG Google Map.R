c569578913ad2450cf246742c37eba0ad771d159
Alathea <alathea@zoology.ubc.ca>
2016-03-18 11:40:50 -0700

    added marker function and small maps for insets

--- BWG Map/BWG Google Map.R
+++ BWG Map/BWG Google Map.R
@@ -13,13 +13,21 @@ library(dplyr)
 sites <- readxl::read_excel("BWG Map/site coordinates.xlsx")
 
 
+# functions ---------------------------------------------------------------
+
+## function to make a RgoogleMaps friendly data frame of markers
+
+getmarkers <- function(df){
+  df %>%
+    select(lat = latitude, lon = longitude) %>%
+    mutate(size  = "small",
+           color = "red",
+           char  = "")
+}
+
 # set up markers ----------------------------------------------------------
 
-mymarkers <- sites %>%
-  select(lat = latitude, lon = longitude) %>%
-  mutate(size = "small",
-         col  = "red",
-         char = "")
+main_markers <- getmarkers(sites)
 
 # create map --------------------------------------------------------------
 
@@ -27,4 +35,26 @@ mymarkers <- sites %>%
 # "satellite", "terrain", "hybrid", "mapmaker-roadmap", "mapmaker-hybrid")
 terrmap <- GetMap(center = c(-20, -70), zoom = 3, maptype = "terrain",
                   size = c(480, 640),
-                  markers = mymarkers, destfile = "BWG Map/terrain.png")
+                  markers = main_markers, destfile = "BWG Map/terrain.png")
+
+# Brazil close up map -----------------------------------------------------
+
+## create a close up of the Brazil sites for inset
+brazil <- sites %>%
+  filter(country == "Brazil") %>%
+  getmarkers()
+
+brazilmap <- GetMap(center = c(-23, -44), zoom = 5, maptype = "terrain",
+                    size = c(240, 200), markers = brazil,
+                    destfile = "BWG Map/brazil.png")
+
+# Colombia close up map ---------------------------------------------------
+
+## create a close up of the Colombia sites for inset
+colombia <- sites %>%
+  filter(country == "Colombia") %>%
+  getmarkers()
+
+colombiamap <- GetMap(center = c(4.767, -74.167), zoom = 7, maptype = "terrain",
+                      size = c(240, 200), markers = colombia,
+                      destfile = "BWG Map/colombia.png")
