c3499844dafd2fe0bc4e8f6cd0a5b4dd8781b19d
Alathea <alathea@zoology.ubc.ca>
2016-03-18 11:29:38 -0700

    added  markers

--- BWG Map/BWG Google Map.R
+++ BWG Map/BWG Google Map.R
@@ -1,10 +1,11 @@
-# Create a BWG field site map using the terrain maps from Google
-
+## Create a BWG field site map using the terrain maps from Google
+## Developed from http://www.molecularecologist.com/2012/09/making-maps-with-r/
 
 # load packages -----------------------------------------------------------
 
 library(ggplot2)
 library(RgoogleMaps)
+library(dplyr)
 
 
 # load file ---------------------------------------------------------------
@@ -12,10 +13,18 @@ library(RgoogleMaps)
 sites <- readxl::read_excel("BWG Map/site coordinates.xlsx")
 
 
-## Developed from http://www.molecularecologist.com/2012/09/making-maps-with-r/
+# set up markers ----------------------------------------------------------
+
+mymarkers <- sites %>%
+  select(lat = latitude, lon = longitude) %>%
+  mutate(size = "small",
+         col  = "red",
+         char = "")
+
+# create map --------------------------------------------------------------
 
 # lots of visual options, just like google maps: maptype = c("roadmap", "mobile",
 # "satellite", "terrain", "hybrid", "mapmaker-roadmap", "mapmaker-hybrid")
-terrmap <- GetMap(center = c(lat = -20, lon = -80), zoom = 3, maptype= "terrain",
-                  destfile = "BWG Map/terrain.png")
-
+terrmap <- GetMap(center = c(-20, -70), zoom = 3, maptype = "terrain",
+                  size = c(480, 640),
+                  markers = mymarkers, destfile = "BWG Map/terrain.png")
