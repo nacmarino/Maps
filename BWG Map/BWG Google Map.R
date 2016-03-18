37ab6b02b568e218eacd9100420858c6c98e474f
Alathea <alathea@zoology.ubc.ca>
2016-03-18 10:32:24 -0700

    initialized google map script

---
+++ BWG Map/BWG Google Map.R
@@ -0,0 +1,13 @@
+# Create a BWG field site map using the terrain maps from Google
+
+# Load libraries
+library(ggplot2)
+library(RgoogleMaps)
+
+## Developed from http://www.molecularecologist.com/2012/09/making-maps-with-r/
+
+# lots of visual options, just like google maps: maptype = c("roadmap", "mobile",
+# "satellite", "terrain", "hybrid", "mapmaker-roadmap", "mapmaker-hybrid")
+terrmap <- GetMap(center = c(lat = -20, lon = -80), zoom = 3, maptype= "terrain",
+                  destfile = "BWG Map/terrain.png")
+
